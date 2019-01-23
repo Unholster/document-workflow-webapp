113a58735d669ba7a4f1132a72bcd90c493bca65
Luis Bustos <lpbustoscarrasco@gmail.com>
2019-01-23 10:29:23 -0300

    Added visualization to doc state and buttons for possible transitions

--- src/App.js
+++ src/App.js
@@ -4,39 +4,11 @@ import Workflow from './components/Workflow/Workflow';
 import Document1 from './components/Documents/Document1Edit';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      submitted: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ submitted: false });
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState({ submitted: true });
-  }
-
   render() {
-    const workflow = this.state.submitted ? <Workflow doc_id={this.state.value} /> : '';
     return (
       <div className="App">
         <Document1 />
-        <Workflow />
-        <form onSubmit={this.handleSubmit}>
-          Document id:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <input type="submit" value="Submit" />
-        </form>
-        <br />
-        { workflow }
+        <Workflow doc_id={14} />
       </div>
     );
   }
