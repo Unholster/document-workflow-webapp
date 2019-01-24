f6ffadd8a775f06b9c8bb6cf733fd58be95233d4
Mortup <gonzalo.uribe@unholster.com>
2019-01-24 12:15:20 -0300

    Restored tests

--- src/__tests__/documentForm.test.js
+++ src/__tests__/documentForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure } from 'enzyme';
+import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Adapter from 'enzyme-adapter-react-16';
 import Document1 from '../components/Documents/Document1Edit';
@@ -7,13 +8,12 @@ import Document1 from '../components/Documents/Document1Edit';
 configure({ adapter: new Adapter() });
 
 test('Both document sections are rendered', () => {
-  const component = mount(
+  const { getByPlaceholderText } = render(
     <Document1 />,
   );
 
-  console.log(component.find({ id: 'first_name' }).first().props());
-  // expect.anything(getByPlaceholderText('Nombre'));
-  // expect.anything(getByPlaceholderText('Apellido'));
-  // expect.anything(getByPlaceholderText('100.000'));
-  // expect.anything(getByPlaceholderText('Descripción aproximada del detalle del trabajo.'));
+  expect.anything(getByPlaceholderText('Nombre'));
+  expect.anything(getByPlaceholderText('Apellido'));
+  expect.anything(getByPlaceholderText('100.000'));
+  expect.anything(getByPlaceholderText('Descripción aproximada del detalle del trabajo.'));
 });
